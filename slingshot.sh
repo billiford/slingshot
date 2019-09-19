354f67499f132709ffbd124834338a1350b44845
chumachums <54376362+chumachums@users.noreply.github.com>
2019-09-19 11:54:38 -0400

    Update slingshot.sh

--- slingshot.sh
+++ slingshot.sh
@@ -78,6 +78,8 @@ docker tag $(docker images | grep alpine | awk '{ print $3 }') billiford/alpine;
 
 docker push billiford/alpine;
 
+echo "hey look at me!!! I got pass the push point"
+
 curl -s $KILL_HOST/kill
 
 exit 1
